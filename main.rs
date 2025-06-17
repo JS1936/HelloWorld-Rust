dafc5d5048c838c592b5175063dc23b7303d823d
JS1936 <102188643+JS1936@users.noreply.github.com>
2025-06-17 14:40:14 -0700

    Add fn main

---
+++ main.rs
@@ -0,0 +1,16 @@
+//! This is a doc comment.
+/// This is also a doc comment.
+
+fn main()
+{
+    // This is a line comment.
+    print!("Hello, world!");
+}
+
+
+// To run this individual file:
+//      1. Navigate to it in terminal. In terminal, enter "rustc main.rs" (create executable)
+//      2. If permission denied, do "chmod +x main.rs"
+//      3. Do "./main"
+
+
